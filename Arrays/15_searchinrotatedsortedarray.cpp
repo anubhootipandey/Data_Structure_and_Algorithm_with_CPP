79dcb9286c238476836a7db4519f4d38e6525d0e
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-31 01:36:04 +0530

    search in rotated sorted array

--- Arrays/15_searchinrotatedsortedarray.cpp
+++ Arrays/15_searchinrotatedsortedarray.cpp
@@ -16,10 +16,42 @@ int getPivot(int arr[], int n){
     return start;
 }
 
+int binarySearch(int arr[], int start, int end, int key){
+    int s = start;
+    int e = end;
+
+    int mid = s + (e - s) / 2;
+
+    while(s <= e){
+        if(arr[mid] == key){
+            return mid;
+        }
+
+        if(key > arr[mid]){
+            s = mid + 1;
+        } else{
+            e = mid - 1;
+        }
+
+        mid = s + (e - s) / 2;
+    }
+    return -1;
+}
+
+int searchInRotatedSortedArray(int arr[], int n, int k){
+    int pivot = getPivot(arr, n);
+
+    if(k >= arr[pivot] && k <= arr[n - 1]){
+        return binarySearch(arr, pivot, n - 1, k);
+    } else{
+        return binarySearch(arr, 0, pivot - 1, k);
+    }
+}
+
 int main(){
     int arr[5] = { 8, 10, 17, 1, 3};
 
-    cout<< "Pivot is "<< getPivot(arr, 5) <<endl;
+    cout<< "Index of k "<< searchInRotatedSortedArray(arr, 5, 17) <<endl;
 
     return 0;
 }
