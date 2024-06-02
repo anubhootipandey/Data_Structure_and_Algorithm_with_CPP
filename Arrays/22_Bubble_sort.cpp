52e1f6b4db5a92211c37d81bfa5f86afb9def60f
anubhootipandey <anubhootipandey05@gmail.com>
2024-06-03 01:04:49 +0530

    BubbleSort

--- Arrays/22_Bubble_sort.cpp
+++ Arrays/22_Bubble_sort.cpp
@@ -4,19 +4,30 @@ using namespace std;
 void bubbleSort(int arr[], int n){
     for(int i=1; i<n; i++){
         //for round 1 to n-1
-        for(int j=0; j< n-1; j++){
+        for(int j=0; j< n-i; j++){
             if(arr[j] > arr[j+1]){
-                swap(arr[j], arr[j+1])
-            }
-        }
-    }
+                swap(arr[j], arr[j+1]);
+            };
+        };
+    };
+};
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<< arr[i] << " ";
+    };
+    cout<< endl;
 }
 
 int main(){
     int arr[] = {29, 72, 98, 13, 87, 66, 52, 52, 36};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout<< "Bubble Sort : "<<bubbleSort(arr, n)<<endl;
+    bubbleSort(arr, n);
+
+    cout<< "Bubble Sort : ";
+    printArray(arr, n);
 
     return 0;
 }
+
