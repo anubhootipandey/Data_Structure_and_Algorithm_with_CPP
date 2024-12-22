af5b055acac6784c13cb28c6f3a7db44ab6fd68c
anubhootipandey <anubhootipandey05@gmail.com>
2024-12-22 21:45:29 +0530

    ASCII Values

--- C++/06_CharLowerOrUpperCase.cpp
+++ C++/06_CharLowerOrUpperCase.cpp
@@ -6,10 +6,17 @@ int main(){
     cout<<"Enter character ch: "<<endl;
     cin >> ch;
 
-    if(ch >= 'a' && ch <= 'z'){
-        cout<< "Character is Lowercase.";
-    } else{
+    // if(ch >= 'a' && ch <= 'z'){
+    //     cout<< "Character is Lowercase.";
+    // } else{
+    //     cout<< "Character is Uppercase.";
+    // }
+
+    //Using ASCII Values
+    if(ch >= 65 && ch <= 90){
         cout<< "Character is Uppercase.";
+    } else{
+        cout<< "Character is Lowercase.";
     }
     return 0;
 }
