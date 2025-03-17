87ed811902a09a83080f5b2e395340b4c0b5b0ea
anubhootipandey <anubhootipandey05@gmail.com>
2025-03-17 21:54:47 +0530

    GCD and LCM problem

---
+++ 04_MathsForDSA/04_GCD.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b){
+    while(a > 0 && b > 0){
+        if(a > b){
+            a = a % b;
+        } else {
+            b = b % a;
+        }
+    }
+
+    if(a == 0) return b;
+    return a;
+}
+
+//gcd recursion code
+
+int gcdRec(int a, int b){
+    if(b == 0) return a;
+     return gcdRec(b, a % b);
+}
+
+int main(){
+    cout << gcdRec(20, 28);
+    return 0;
+}
