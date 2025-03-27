f4eae854c2f43bc4a669195e0041d9556e7a3dd6
anubhootipandey <anubhootipandey05@gmail.com>
2025-03-27 20:24:18 +0530

    print numbers

---
+++ 07_Recursion/00_PrintNumbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printNumbers(int n){
+
+    if(n == 1){
+        cout << "1\n";
+        return;
+    }
+    cout << n << " ";
+    printNumbers(n-1);
+}
+
+int main(){
+    printNumbers(30);
+    return 0;
+}
